dcb34b733f3c20b6aec4af354f3ff96401eac029
Jonatan Murcia <jonatanjosmurcia@gmail.com>
2021-04-19 21:11:16 -0500

    Inicio del proyecto

---
+++ src/main/java/com/projectSpring/projectmarket/ProjectMarketApplication.java
@@ -0,0 +1,14 @@
+package com.projectSpring.projectmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMarketApplication.class, args);
+	}
+
+
+}
