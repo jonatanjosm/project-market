6f315a87219d9d30ff1bfaaef588d2188d78e341
Jonatan Murcia <jonatanjosmurcia@gmail.com>
2021-04-20 09:25:25 -0500

    Persistence and repository

---
+++ src/main/java/com/projectSpring/projectmarket/persistence/ProductoRepository.java
@@ -0,0 +1,51 @@
+package com.projectSpring.projectmarket.persistence;
+
+import com.projectSpring.projectmarket.domain.Product;
+import com.projectSpring.projectmarket.domain.repository.ProductRepository;
+import com.projectSpring.projectmarket.persistence.crud.ProductoCrudRepository;
+import com.projectSpring.projectmarket.persistence.entity.Producto;
+import com.projectSpring.projectmarket.persistence.mapper.ProductMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ProductoRepository implements ProductRepository {
+    private ProductoCrudRepository productoCrudRepository;
+    private ProductMapper mapper;
+
+    @Override
+    public List<Product> getAll(){
+        List<Producto> productos = (List<Producto>) productoCrudRepository.findAll();
+        return mapper.toProducts(productos);
+    }
+
+    @Override
+    public Optional<List<Product>> getByCategory(int categoryId) {
+        List<Producto> productos = productoCrudRepository.findByIdCategoriaOrderByNombreAsc(categoryId);
+        return Optional.of(mapper.toProducts(productos));
+    }
+
+    @Override
+    public Optional<List<Product>> getScarseProducts(int quantity) {
+        Optional<List<Producto>> productos = productoCrudRepository.findByCantidadStockLessThanAndEstado(quantity, true);
+        return productos.map(prods -> mapper.toProducts(prods));
+    }
+
+    @Override
+    public Optional<Product> getProduct(int productId) {
+        return productoCrudRepository.findById(productId).map(producto -> mapper.toProduct(producto));
+    }
+
+    @Override
+    public Product save(Product product) {
+        Producto producto = mapper.toProducto(product);
+        return mapper.toProduct(productoCrudRepository.save(producto));
+    }
+
+    @Override
+    public void delete(int productId){
+        productoCrudRepository.deleteById(productId);
+    }
+}
